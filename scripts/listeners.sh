0363cd221d0af954ebf7f4c9bb4fbbdf84d596b9
Scuttle <gustav@almstrom.org>
2021-05-06 14:20:10 +0200

    stack

--- scripts/listeners.sh
+++ scripts/listeners.sh
@@ -2,6 +2,6 @@
 
 while true
 do
-	curl -s http://admin:adminpassword@192.168.0.154:8888/admin/stats| awk -F"[<>]" '{print $33}'|tr -d '\n' > /home/gbsfm/listeners.txt
+	curl -s http://username:password@192.168.0.154:8888/admin/stats| awk -F"[<>]" '{print $33}'|tr -d '\n' > /home/gbsfm/listeners.txt
 	sleep 30
 done
