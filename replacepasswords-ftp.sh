70231bd8343922d7aea92667f0c8ffa9cc3ca8e7
Scuttle <gustav@almstrom.org>
2021-05-07 15:27:24 +0200

    stack

---
+++ replacepasswords-ftp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "DATABASE_HOST = '$DRONE_DATABASE_HOST'" > /srv/pydj/other_settings.py
+echo "DATABASE_PASSWORD = '$DRONE_DATABASE_PASSWORD'" >> /srv/pydj/other_settings.py
+echo "DATABASE_USERNAME = '$DRONE_DATABASE_USERNAME'" >> /srv/pydj/other_settings.py
+echo "NEXT_PASSWORD = '$DRONE_NEXT_PASSWORD'" >> /srv/pydj/other_settings.py
+echo "SA_PASSWORD = '$DRONE_SA_PASSWORD'" >> /srv/pydj/other_settings.py
+echo "SA_USERNAME = '$DRONE_SA_USERNAME'" >> /srv/pydj/other_settings.py
+echo "SECRET_KEY = '$DRONE_SECRET_KEY'" >> /srv/pydj/other_settings.py
+
+
+
