f4892854a1e486c5b08550ccb143503b6abc9946
Scuttle <gustav@almstrom.org>
2021-12-16 14:15:29 +0100

    fixes for python3

---
+++ scripts/remaining3.py
@@ -0,0 +1,31 @@
+#!/srv/pydj/bin/python
+import pymysql.cursors
+import telnetlib
+import time
+
+telnethost = ''
+cuefile='/remaining/ices2.cue'
+
+connection = pymysql.connect(host='', user='', password='', db='gbsfm', charset='utf8', cursorclass=pymysql.cursors.DictCursor, autocommit=True)
+
+while True:
+    query = connection.cursor()
+    query.execute ("SELECT playlist_song.length \
+			    FROM playlist_playlistentry \
+			    INNER JOIN playlist_song \
+			    ON playlist_playlistentry.song_id = playlist_song.id \
+			    WHERE playlist_playlistentry.playing = 1")
+    queryresult = query.fetchone()
+    tn = telnetlib.Telnet(telnethost, 1234)
+    tn.write(b'96.remaining\n')
+    tn.write(b'quit\n')
+    telnetresult = tn.read_all().decode('ascii')
+
+    try:
+        with open(cuefile, 'w') as f:
+    	    f.write('dummyline1\ndummyline2\ndummyline3\ndummyline4\n')
+    	    f.write(str(100 - (float(telnetresult.split()[0]) / int(queryresult['length']) * 100)))
+    	    f.write('\n0\ndummyline7\ndummyline8')
+    except (ValueError, TypeError) as e:
+    	print('Oopsie')
+    time.sleep(1)
