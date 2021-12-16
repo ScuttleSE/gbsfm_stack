f253b92e8686383bbd4d4b26e29eb962bdeb4d5c
Scuttle <gustav@almstrom.org>
2021-12-16 20:14:40 +0100

    fix for python3 image

--- replacepasswords3.sh
+++ replacepasswords3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "DATABASE_HOST = '$DRONE_DATABASE_HOST'" > /srv/pydj/olitest/other_settings.py
-echo "DATABASE_PASSWORD = '$DRONE_DATABASE_PASSWORD'" >> /srv/pydj/olitest/other_settings.py
-echo "DATABASE_USERNAME = '$DRONE_DATABASE_USERNAME'" >> /srv/pydj/olitest/other_settings.py
-echo "NEXT_PASSWORD = '$DRONE_NEXT_PASSWORD'" >> /srv/pydj/olitest/other_settings.py
-echo "SA_PASSWORD = '$DRONE_SA_PASSWORD'" >> /srv/pydj/olitest/other_settings.py
-echo "SA_USERNAME = '$DRONE_SA_USERNAME'" >> /srv/pydj/olitest/other_settings.py
-echo "SECRET_KEY = '$DRONE_SECRET_KEY'" >> /srv/pydj/olitest/other_settings.py
-echo "DRONE_CI = '$DRONE_CI'" >> /srv/pydj/olitest/other_settings.py
+echo "DATABASE_HOST = '$DRONE_DATABASE_HOST'" > /srv/pydj/gbsfm/other_settings.py
+echo "DATABASE_PASSWORD = '$DRONE_DATABASE_PASSWORD'" >> /srv/pydj/gbsfm/other_settings.py
+echo "DATABASE_USERNAME = '$DRONE_DATABASE_USERNAME'" >> /srv/pydj/gbsfm/other_settings.py
+echo "NEXT_PASSWORD = '$DRONE_NEXT_PASSWORD'" >> /srv/pydj/gbsfm/other_settings.py
+echo "SA_PASSWORD = '$DRONE_SA_PASSWORD'" >> /srv/pydj/gbsfm/other_settings.py
+echo "SA_USERNAME = '$DRONE_SA_USERNAME'" >> /srv/pydj/gbsfm/other_settings.py
+echo "SECRET_KEY = '$DRONE_SECRET_KEY'" >> /srv/pydj/gbsfm/other_settings.py
+echo "DRONE_CI = '$DRONE_CI'" >> /srv/pydj/gbsfm/other_settings.py
 
 sed -i "s/username:password/$DRONE_LISTENERS/g" /home/gbsfm/listeners.sh
 
