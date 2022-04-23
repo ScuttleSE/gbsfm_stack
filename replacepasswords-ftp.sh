3082264ed35b1e6f07d9552d91caf2919ec396b9
Scuttle <gustav@almstrom.org>
2022-04-23 09:42:14 +0200

    updated ftp

--- replacepasswords-ftp.sh
+++ replacepasswords-ftp.sh
@@ -7,3 +7,4 @@ echo "NEXT_PASSWORD = '$DRONE_NEXT_PASSWORD'" >> /srv/pydj/gbsfm/other_settings.
 echo "SA_PASSWORD = '$DRONE_SA_PASSWORD'" >> /srv/pydj/gbsfm/other_settings.py
 echo "SA_USERNAME = '$DRONE_SA_USERNAME'" >> /srv/pydj/gbsfm/other_settings.py
 echo "SECRET_KEY = '$DRONE_SECRET_KEY'" >> /srv/pydj/gbsfm/other_settings.py
+echo "DRONE_CI = '$DRONE_CI'" >> /srv/pydj/gbsfm/other_settings.py
